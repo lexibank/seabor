78e710b298a37cb8b95c7994aebe15439f31c623
lingulist <mattis.list@lingpy.org>
2021-08-18 21:09:50 +0200

    update workflow and new code for tests

--- seaborcommands/fullcomparison.py
+++ seaborcommands/fullcomparison.py
@@ -1,12 +1,16 @@
 """
 Compare the alternative method for borrowing detection by Hantgan et al. 2020.
 """
+import random
 from lingpy import *
+from lingpy.compare.partial import Partial
 from lexibank_seabor import Dataset as sb
 from lingpy.evaluate.acd import bcubes
 from clldutils.clilib import Table, add_format
 from lingrex import cognates, borrowing
 from tabulate import tabulate
+from matplotlib import pyplot as plt
+
 
 
 def register(parser):
@@ -17,21 +21,14 @@ def register(parser):
             action="store_true",
             default=False)
     parser.add_argument(
-            "--lingrex",
-            help="use lingrex algorithm",
-            action="store_true",
-            default=False)
-    parser.add_argument(
             "--partial",
             help="use partial lexstat algorithm for cognate detection",
             action="store_true",
             default=False)
 
 
-
-
-
 def run(args):
+    random.seed(int(input('Random seed [int]: ') or 1234))
     if args.lexstat:
         method = "lexstat"
     else:
@@ -40,82 +37,98 @@ def run(args):
         from igraph import Graph
         cluster_method = "infomap"
     except:
-        cluster_method="upgma"
+        cluster_method = "upgma"
+        args.log.warning("Using UPGMA as cluster method")
 
-    if args.lingrex: 
-        table = []
-        wl = Wordlist(str(sb().raw_dir / "seabor.sqlite3"))
-        for i, t in enumerate([0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7]):
-            ref = "cog-{0}".format(i)
-            borrowing.internal_cognates(
-                    wl,
-                    partial=args.partial,
-                    runs=10000,
-                    ref=ref,
-                    method="lexstat",
-                    threshold=t,
+    table = []
+    if args.partial:
+        lex = Partial(str(sb().raw_dir / "seabor.sqlite3"))
+        args.log.info("loading partial object")
+    else:
+        lex = LexStat(str(sb().raw_dir / "seabor.sqlite3"))
+        args.log.info("loading lexstat object")
+
+    # renumber borrowings !
+    clusterid = max([int(x) for x in lex.get_etymdict("uborid")])+1
+    for idx in lex:
+        if lex[idx, "uborid"] == "0":
+            lex[idx, "uborid"] = clusterid
+            clusterid += 1
+
+    # add lumper and splitter baseline
+
+    if method == "lexstat":
+        if not args.partial:
+            lex.get_scorer(runs=10000)
+        else:
+            lex.get_partial_scorer(runs=10000)
+
+    for i, t in enumerate([0.05 * j for j in range(1, 20)]):
+        if args.partial:
+            lex.partial_cluster(
+                    method=method, threshold=t,
+                    ref="scallids_{0}".format(i),
                     cluster_method=cluster_method)
-            if args.partial:
-                cognates.common_morpheme_cognates(
-                        wl,
-                        ref=ref+"id",
-                        cognates=ref,
-                        morphemes=ref+"-morphemes")
-            else:
-                wl.add_entries(ref+"id", ref, lambda x: x)
-
-            # renumber wordlist to place 0 into their own cluster
-            clusterid = max(wl.get_etymdict(ref+"id"))+1
-            for idx in wl:
-                if wl[idx, ref+"id"] == 0:
-                    wl[idx, ref+"id"] = clusterid
+            cognates.common_morpheme_cognates(
+                lex,
+                ref="scallid_{0}".format(i),
+                cognates="scallids_{0}".format(i),
+                morphemes="automorphemes_{0}".format(i))
+        else:
+            lex.cluster(method=method, threshold=t,
+                    ref="scallid_{0}".format(i), cluster_method=cluster_method)
+        lex.add_entries("sca_{0}".format(i), "scallid_{0},family".format(i), lambda x, y:
+                str(x[y[0]])+"-"+x[y[1]])
+        lex.renumber("sca_{0}".format(i))
+        etd = lex.get_etymdict(ref="scallid_{0}".format(i))
+        nulls = {}
+        clusterid = max(etd)+1
+        for cogid, vals in etd.items():
+            idxs = []
+            for v in vals:
+                if v:
+                    idxs += v
+            famis = [lex[idx, 'family'] for idx in idxs]
+            if len(set(famis)) == 1:
+                for idx in idxs:
+                    lex[idx, 'scallid_{0}'.format(i)] = clusterid
                     clusterid += 1
 
-            p1, r1, f1 = evaluate.acd.bcubes(wl, "ucogid", ref+"id", pprint=False)
-            table += [["cognates", t, 0, p1, r1, f1]]
-            for j, t2 in enumerate([0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]):
-                borrowing.external_cognates(
-                        wl,
-                        cognates=ref+"id",
-                        ref=ref+"{0}-borid".format(j),
-                        threshold=t2)
-                p2, r2, f2 = evaluate.acd.bcubes(
-                        wl, "uborid", ref+"{0}-borid".format(j), pprint=False)
-                table += [[t, t2, p1, r1, f1, p2, r2, f2]]
-        with Table(
-                args, "Threshold A", "Threshold B", "P1", "R1", "F1", "P2", "R2", "F2") as tab:
-            for row in table:
-                tab.append(row)
-    else:
-        table = []
-        lex = LexStat(str(sb().raw_dir / "seabor.sqlite3"))
-        if method == "lexstat":
-            lex.get_scorer(runs=10000)
+        p1, r1, f1 = bcubes(lex, "ucogid", "sca_{0}id".format(i), pprint=False)
+        p2, r2, f2 = bcubes(lex, "uborid", "scallid_{0}".format(i), pprint=False)
+        table += [[t, p1, r1, f1, p2, r2, f2]]
+    with Table(
+            args, "Threshold", "P1", "R1", "F1", "P2", "R2", "F2",
+            floatfmt=".4f") as tab:
+        for row in table:
+            tab.append(row)
 
-        for i, t in enumerate([0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.85, 0.9]):
-            args.log.info("loaded data")
-            lex.cluster(method=method, threshold=t, ref="scallid_{0}".format(i))
-            lex.add_entries("sca_{0}".format(i), "scallid_{0},family".format(i), lambda x, y:
-                    str(x[y[0]])+"-"+x[y[1]])
-            lex.renumber("sca_{0}".format(i))
-            etd = lex.get_etymdict(ref="scallid_{0}".format(i))
-            nulls = {}
-            clusterid = max(etd)+1
-            for cogid, vals in etd.items():
-                idxs = []
-                for v in vals:
-                    if v:
-                        idxs += v
-                famis = [lex[idx, 'family'] for idx in idxs]
-                if len(set(famis)) == 1:
-                    for idx in idxs:
-                        lex[idx, 'scallid_{0}'.format(i)] = clusterid
-                        clusterid += 1
+    fig = plt.Figure()
+    plt.plot(
+            1, table[0][3], 'o', color="Crimson", 
+            label="family-internal cognates")
+    plt.plot(
+            1, table[0][-1], 'o', color="CornFlowerBlue",
+            label="family-external xenologs")
 
-            p1, r1, f1 = bcubes(lex, "ucogid", "sca_{0}id".format(i), pprint=False)
-            p2, r2, f2 = bcubes(lex, "uborid", "scallid_{0}".format(i), pprint=False)
-            table += [[t, p1, r1, f1, p2, r2, f2]]
-        with Table(args, "Threshold", "P1", "R1", "F1", "P2", "R2", "F2") as tab:
-            for row in table:
-                tab.append(row)
+    for i, row in enumerate(table[1:]):
+        plt.plot(i+2, row[3], 'o', color="Crimson")
+        plt.plot(i+2, row[-1], 'o', color="CornFlowerBlue")
+    plt.xticks(
+            list(
+                range(1, 20)
+                ), ["{0:.2f}".format(i*0.05) for i in range(1, 20)], rotation=90)
+    plt.yticks([i*0.1 for i in range(3, 10)])
+    plt.ylim(0.3, 1.0)
+    plt.xlabel("cognate detection thresholds")
+    plt.ylabel("B-cubed F-scores")
+    plt.title("Cognate vs. Xenolog Detection ({0}, {1})".format(
+        "LexStat" if args.lexstat else "SCA",
+        "Full Cognates" if not args.partial else "Partial Cognates"))
+    plt.legend(loc=4)
+    plt.savefig("plots/full_comparison-{0}-{1}.pdf".format(
+        "lexstat" if args.lexstat else "sca",
+        "full" if not args.partial else "partial"
+        ))
+
     
